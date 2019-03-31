f52ba3836dcdf81c212ca10618a3221c5ad85bd2
GHLandy <ghlandy@ghlandy.com>
2019-03-31 16:03:23 +0800

    index page

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,29 +1,55 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View, Button, Text } from '@tarojs/components'
+
 import Logo from '../../components/Logo'
 import './index.less'
 
 export default class Index extends Component {
-
   config = {
-    navigationBarTitleText: 'Logoly.Pro'
+    navigationBarTitleText: 'Logoly.Pro',
   }
 
-  componentWillMount () { }
+  componentWillMount() {}
 
-  componentDidMount () { }
+  componentDidMount() {}
 
-  componentWillUnmount () { }
+  componentWillUnmount() {}
 
-  componentDidShow () { }
+  componentDidShow() {}
 
-  componentDidHide () { }
+  componentDidHide() {}
 
-  render () {
+  goToCustomize = () => Taro.navigateTo({ url: '/pages/generator/index' })
+
+  render() {
     return (
-      <View className='logoly-pro index'>
+      <View className="logoly-pro index">
         <Logo />
-        <View className='logoly-pro-brief'>一个简约的在线 Logo 生成工具</View>
+        <View className="logoly-pro-brief">一个简约的在线 Logo 生成工具</View>
+
+        <Button className="go-to-customize" onClick={this.goToCustomize} type="primary" plain>
+          马上定制 Logo
+        </Button>
+
+        <View className="logoly-pro-footer">
+          <View className="footer-item">
+            <Text className="desc">Inspiration by Bestony</Text>
+            <Image
+              style="width: 16px; height: 16px; border-radius: 8px; vertical-align: middle;"
+              src="https://avatars3.githubusercontent.com/u/13283837?s=32&v=4"
+            />
+          </View>
+          <View className="footer-item">
+            <Text className="desc">Code by GHLandy</Text>
+            <Image
+              style="width: 16px; height: 16px; border-radius: 8px; vertical-align: middle;"
+              src="https://avatars3.githubusercontent.com/u/14830914?s=32&v=4"
+            />
+          </View>
+          <View className="footer-item">
+            <Text className="desc desc-link">Repo at https://github.com/GHLandy/logoly-pro.git</Text>
+          </View>
+        </View>
       </View>
     )
   }
